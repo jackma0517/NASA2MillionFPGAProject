b4f405f16dd8d7a2a89738b4f09e55dbad3e8434
Timothy <tl@alumni.ubc.ca>
2017-01-19 22:54:17 -0800

    Fixes Clock Divider by initializing registers to 0 allowing divider to run without resetting.

--- Our_Clk_Divider_32.v
+++ Our_Clk_Divider_32.v
@@ -1,23 +1,24 @@
-module Our_Clk_Divider_32(inclk,outclk,outclk_not,div_clk_count,Reset);
+module Our_Clk_Divider_32(inclk,outclk,outclk_Not,div_clk_count,Reset);
      input inclk;
 	 input Reset;
 	 output outclk;
 	 output outclk_Not;
 	 input[31:0] div_clk_count;
 	 
-	 reg [31:0] counter;
+	 reg [31:0] counter = 0;
 	 wire [31:0] next_value;
-	 reg reg_clk;
+	 reg reg_clk = 0;
 	 
 	 always @(posedge inclk or posedge Reset)
 		begin
 	 
-		if(Reset)
+		if(~Reset)
 			begin
 				counter <= 0;
 				reg_clk <= 1'b0;
 			end
-		else if (next_value == div_clk_count) // counter reached
+		else if (next_value >= div_clk_count) 
+			// Counter reached, reverse clock sig
 			begin
 				counter <= 0;
 				reg_clk <= ~reg_clk;
