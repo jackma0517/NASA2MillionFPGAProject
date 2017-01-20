dfe2bae637e56e6438241cebbaae9792cc71f077
Timothy <tl@alumni.ubc.ca>
2017-01-19 23:01:15 -0800

    Tests that changing the clock divider without reset will change output clock

--- ModelSim/testbench.v
+++ ModelSim/testbench.v
@@ -5,20 +5,33 @@ module testbench();
 
 
 	reg clock_sig;
-        wire clock_out;
-	wire outclk;
+	reg [31:0] divisor;
 	reg reset;
+	
+    wire clock_out;
+	wire outclk;
+	wire [9:0]led_out;
 
-	Our_Clk_Divider_32 clkdivider(clock_sig,outclk,,32'b10,reset);
-
+	Our_Clk_Divider_32 clkdivider(clock_sig,outclk,,divisor,reset);
+	LED_sequence seq(.clock(outclk),.led_out(led_out),.reset(reset));
 	 integer i; 
     initial begin
 		
 		reset = 1'b1;
-		#50
-		reset = 1'b0;
+		divisor = 32'b10;
+		//#50
+		//reset = 1'b0;
 
-        for(i=0; i<20; i=i+1)
+        for(i=0; i<5; i=i+1)
+        begin
+            clock_sig <=1;
+            #10;
+            clock_sig <=0;
+            #10;
+        end
+		
+		divisor = 32'b01;
+        for(i=0; i<5; i=i+1)
         begin
             clock_sig <=1;
             #10;
